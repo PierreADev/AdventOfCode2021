40b009682d25e5be404a8b7eed468a23b33cf4a4
Pierre <58070376+PierreADev@users.noreply.github.com>
2021-12-02 17:02:11 +0100

    nettoyage 2
    1. transformation du forEach en for
    2. précision de la base 10 pour parseInt

--- day1/part1.js
+++ day1/part1.js
@@ -5,15 +5,15 @@ const input = readFileSync("input.data", { encoding: "utf-8" });
 const inputArray = input.split("\n");
 
 const numbersArray = inputArray.map(function(num){
-    return parseInt(num);
+    return parseInt(num, 10);
 });
 
 let counter = 0;
 
-numbersArray.forEach(function(num, i){
+for (const [i, num] of numbersArray.entries()){
     if (numbersArray[i] > numbersArray[i-1]) {
         counter ++;
     }
-});
+};
 
 console.log (counter); 
