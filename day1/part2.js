d81b50885bac98dced8c926b99d63521e587a84b
Pierre <58070376+PierreADev@users.noreply.github.com>
2021-12-02 17:01:35 +0100

    nettoyage
    1. Suppression d'un console.log superflu
    2. transformation du forEach en for
    3. précision de la base 10 dans parseInt

--- day1/part2.js
+++ day1/part2.js
@@ -5,17 +5,15 @@ const input = readFileSync("input.data", { encoding: "utf-8" });
 const inputArray = input.split("\n");
 
 const numbersArray = inputArray.map(function(num){
-    return parseInt(num);
+    return parseInt(num, 10);
 });
-
-console.log(numbersArray);
 
 let counter = 0;
 
-numbersArray.forEach(function(num, i){
+for (const [i, num] of numbersArray.entries()){
     if ((numbersArray[i] > 2) && ((numbersArray[i] + numbersArray[i-1] + numbersArray[i-2]) > numbersArray[i-1] + numbersArray[i-2] + numbersArray[i-3] )) {
         counter ++;
     }
-});
+};
 
 console.log (counter); 
