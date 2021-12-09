730f829ba0a485c110c5fb5f8988b598fb5bf404
PierreADev <pierre.arragain@gmail.com>
2021-12-09 11:58:42 +0100

    day 2

---
+++ Day2/day2part1.js
@@ -0,0 +1,31 @@
+const { readFileSync } = require("fs");
+
+const data = readFileSync("input.data", { encoding: "utf-8"});
+
+const inputArray = data.split("\n");
+
+let horizontal = 0;
+let depth = 0;
+
+for (const line of inputArray){
+
+    const [instruction, value] = line.split(' ');
+
+   console.log([instruction, value]); 
+
+    switch (instruction) {
+        case 'forward':
+            horizontal += parseInt(value, 10);
+            break;
+        case 'down':
+            depth += parseInt(value, 10);
+            break;
+        case 'up': 
+            depth -= parseInt(value, 10);
+            break;
+        default:
+            throw new Error('input error');
+    }
+}
+
+console.log(horizontal * depth);
